1c2d2d611602fcdd55b5fdb49a4646e3c1b669b4
Jason Li <jasonli1337@gmail.com>
2021-01-30 19:42:08 -0500

    Addign animal controller

---
+++ Assets/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalController : MonoBehaviour
+{
+
+    [SerializeField]
+    private int animalCount = 10;
+    [SerializeField]
+    private int foodCount = 10;
+    []
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
